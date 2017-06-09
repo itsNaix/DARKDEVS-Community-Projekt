cc1932e3d31d63ffd7f9620e575bf673fcc79258
Skillkiller <Skillkiller@thecoin.de>
2017-06-09 03:20:45 +0200

    Plugin Main Datei

---
+++ src/main/java/de/darkdevs/cp/main/Main.java
@@ -0,0 +1,20 @@
+package de.darkdevs.cp.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Skillkiller on 09.06.2017.
+ */
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
